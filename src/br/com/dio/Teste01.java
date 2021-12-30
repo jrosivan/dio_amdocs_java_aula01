2d9f15c9808cb9569ec1743c3b62e8d955f8e030
Rosivan Batista <jrosivan@gmail.com>
2021-12-30 20:42:21 -0300

    First commit

---
+++ src/br/com/dio/Teste01.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class Teste01 {
+
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+    }
+
+}

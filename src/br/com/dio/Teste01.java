719654edef2c049441ba3533433234cafaafdbc9
Rosivan Batista <jrosivan@gmail.com>
2021-12-30 20:51:32 -0300

    Update - criando class Livro

--- src/br/com/dio/Teste01.java
+++ src/br/com/dio/Teste01.java
@@ -7,6 +7,44 @@ public class Teste01 {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
+
+        Livro livro = new Livro("O problema dos 3 corpos", 300);
+        System.out.println( livro );
+
     }
 
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
 }
